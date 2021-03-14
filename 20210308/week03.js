4cdf75450ce1ffee1f6569263f0d0ccd1036c24b
hokou <30932790+hokou@users.noreply.github.com>
2021-03-14 16:51:00 +0800

    Add files via upload

--- 20210308/week03.js
+++ 20210308/week03.js
@@ -1,20 +1,40 @@
 
 const url = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json";
 
-fetch(url).then(function(response){
-    return response.json();
-}).then(function(result){
-    result = result.result.results;
-    // console.log(result);
-    // return result;
-    let list=document.querySelector("#row");
-    let ori_length = 0;
-    let add_length = 8;
-    render(list,ori_length,add_length,result);
-});
+// 起始值
+// 第一次顯示數量
+// 結束值
+let ori_length = 0;
+let first_num = 8;
+let end_length = ori_length + first_num;
 
-function render(list,ori_length,add_length,result){
-    for (let i=ori_length;i<ori_length+add_length;i++){
+// 按一次增加的數量
+// JSON 總數量
+let oneclick_num = 8;
+let total_length = 0;
+// total = 319
+
+
+function fetch_url(){
+    fetch(url).then(function(response){
+        return response.json();
+    }).then(function(result){
+        result = result.result.results;
+        total_length = result.length;
+        let list = document.querySelector("#row");
+        if (end_length >= total_length){
+            end_length = total_length;
+        };
+        // console.log("add,tot",end_length,total_length);
+        // console.log(result);
+        render(list,ori_length,end_length,result);
+    });
+};
+
+
+
+function render(list,ori_length,end_length,result){
+    for (let i=ori_length;i<end_length;i++){
         // console.log(result[i].stitle);
         let title = result[i].stitle;
         let img = result[i].file.split("http");
@@ -28,12 +48,19 @@ function render(list,ori_length,add_length,result){
         div_img.className="pic";
         div_title.textContent=title;
         div_img.style.backgroundImage="url(" + img + ")";
-        // console.log('"url(' +img +')"');
         list.appendChild(div_block);
         div_block.appendChild(div_img);
         div_block.appendChild(div_title);
     }
+    // console.log(ori_length,end_length);
 }
 
 
+function loadmore() {
+    ori_length = end_length;
+    end_length += oneclick_num;
+    // console.log("loadmore",ori_length,end_length,oneclick_num);
+    fetch_url();
+}
 
+fetch_url();
